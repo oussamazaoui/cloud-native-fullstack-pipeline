d56e35772b526dc75d3bfe6bc6f0d47ddd0735a0
Oussama Zaoui <133207079+oussamazaoui@users.noreply.github.com>
2023-12-25 05:30:36 +0100

    Update tutorial.service.ts

--- angular-15-client/src/app/services/tutorial.service.ts
+++ angular-15-client/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = environment.API_BASE_URL;
+const baseUrl = '/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
